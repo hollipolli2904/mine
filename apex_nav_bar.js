a7a54279b4783fc6e77e48577d11b7e6f9270290
Holger Lehmann <holger.lehmann@opitz-consulting.com>
2021-05-10 11:51:17 +0200

    Update apex_nav_bar.js

--- apex_nav_bar.js
+++ apex_nav_bar.js
@@ -7,6 +7,12 @@ $('#t_Body_title').css('left', '320px');
 /* hier 48 Pixel abziehen von der Breite beim Content */
 $('#t_Body_content').css('margin-left', '272px');
 
+//Dafür sorgen, dass bei Page Load Navigation aufgeklappt ist
+if ($('.t-PageBody').hasClass('js-navCollapsed')) {
+        $('#t_Button_navControl').click();
+    };
+
+
 apex.jQuery("#t_TreeNav").on('theme42layoutchanged', function(event, obj) {
    // hier Menü einklappen
    if (obj.action == 'collapse') {
@@ -39,11 +45,10 @@ apex.jQuery("#t_TreeNav").on('theme42layoutchanged', function(event, obj) {
     
 });
 
-(function($) {
-    
+// theme42ready is triggered after the initialization of Universal Theme:
+(function($) {    
 $(window).on('theme42ready', function() {
-
-    /* Expand on mouse over, collapse on mouse out */
+    /* Expand on mouse click, collapse on mouse click */
     $('.t-Body-nav').click(
         function(){
             //only expand if the side menu is collapsed
